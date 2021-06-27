a62ad5cae9a7efb535d700276fba7bfa3741677a
ravshanyusupov <84452409+ravshanyusupov@users.noreply.github.com>
2021-06-27 11:32:19 +0500

    topshiriq

---
+++ 3-topshiriq.py
@@ -0,0 +1,107 @@
+
+1111111111111111111111111111111
+
+# a = int(input("son kiriting: "))
+# b = 0
+# for i in range(a+1):
+#     b+=(i*i)
+    
+# print(b)
+
+
+
+2222222222222222222222222
+
+
+# a = int(input(" a son kirirting: "))
+# b = int(input(" b son kirirting: "))
+# for c in range(a, b, 1):
+#     if a <= b:
+#      print(c)
+# else: 
+#     print("xato son kiritildi")
+
+
+
+
+
+4444444444444444444444
+
+
+# a = int(input("son: "))
+# b = 0
+# for x in range(0, a, 2):
+#  b+=(x+x)
+#  print(b)
+
+
+
+55555555555555555555
+
+# a = int(input("son: "))
+# b = 0
+# for x in range(a):
+#  b+=(x+x)/a
+#  print(b)
+
+
+
+
+6666666666666666666
+
+# from math import *
+
+# a = int(input("son kiriting: "))
+# b = 0
+# for i in range(a+1):
+#     b+= sqrt(i*i)
+    
+# print(b)
+
+
+8888888888888888888888888
+
+
+# a = 1
+# b = 2*10
+# c = 3*100
+# d = a + b + c
+# print(d)
+
+
+
+9999999999999999999999999
+
+
+# for x in range(13):
+#     if x == 1 or x ==3 or x == 5 or x == 6 or x == 8 or x == 10 or x == 12:
+#         print(sep=" ")
+#         print(x," " ,end="  ")
+#         for y in range(1,32,1):
+#             print(y,end='  ')
+
+
+
+10
+
+
+
+# a = int(input("son: "))
+# b = 22
+# c  = 3
+# e = 0
+# n = (a/100)*b
+# if a < 100:
+#     print("xatolik")
+# else:
+#     for x in range(1, c+1, 1):
+#         e+=n
+
+# print("jami", e + a)
+# print("foyda", e)
+
+
+
+
+
+
